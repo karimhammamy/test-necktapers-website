bfe7d3f6b09faaacfd79796351e2e45b52b3874d
karimhammamy <159165220+karimhammamy@users.noreply.github.com>
2024-06-12 01:31:14 +0300

    Update autoplay-slider-video-low-power-mode.js

--- assets/js/autoplay-slider-video-low-power-mode.js
+++ assets/js/autoplay-slider-video-low-power-mode.js
@@ -1,6 +1,14 @@
 sliderBackgroundVideo = document.getElementById("slider-background-video");
+function checkVideoStatus(){
+  if(sliderBackgroundVideo.paused =true){
+    sliderBackgroundVideo.style.display = "none";
+  }
+
+}
+
 function autoplay(){
   sliderBackgroundVideo.play();
+  sliderBackgroundVideo.style.display = "inline";
 }
 window.onload = autoplay;
 window.ontouchstart = autoplay;
