3439ceeaabda9081f46779126a8326d82763f598
karimhammamy <159165220+karimhammamy@users.noreply.github.com>
2024-06-12 01:04:53 +0300

    Update autoplay-slider-video-low-power-mode.js

--- assets/js/autoplay-slider-video-low-power-mode.js
+++ assets/js/autoplay-slider-video-low-power-mode.js
@@ -1,26 +1,5 @@
-Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
-  get: function () {
-      return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
-  }
-});
 sliderBackgroundVideo = document.getElementById("slider-background-video");
-sliderBackgroundVideo.addEventListener('suspend', () => {
-  // suspend invoked
-  // show play button
-});
-
-sliderBackgroundVideo.addEventListener('play', () => {
-  // video is played
-  // remove play button UI
-});
-$('body').on('click touchstart', function () {
-  const sliderBackgroundVideo = document.getElementById('home_video');
-  if (sliderBackgroundVideo.playing) {
-      // video is already playing so do nothing
-  }
-  else {
-      // video is not playing
-      // so play video now
-      sliderBackgroundVideo.play();
-  }
-});
+function autoplay(){
+  sliderBackgroundVideo.play();
+}
+window.ontouchstart = autoplay;
