1cfc11096e80153762a55713f535bf76033af3d0
karimhammamy <159165220+karimhammamy@users.noreply.github.com>
2024-06-12 02:09:42 +0300

    Update autoplay-slider-video-low-power-mode.js

--- assets/js/autoplay-slider-video-low-power-mode.js
+++ assets/js/autoplay-slider-video-low-power-mode.js
@@ -1,10 +1,18 @@
- sliderBackgroundVideo = document.getElementById("slider-background-video");
-  videoState = sliderBackgroundVideo.paused;
-  if(videoState = false){
+var sliderBackgroundVideo = document.getElementById("slider-background-video");
+var promise = sliderBackgroundVideo.play();
+  if(promise !== undefined){
+    promise.catch(error => {
+      //Auto-play was prevented
+      //Show a UI element to let the user manually start playback
+      if (error.name === "NotAllowedError"){
+        console.log("Low Power Mode Active");
+        sliderBackgroundVideo.remove();
+      }
+    })
     sliderBackgroundVideo.style.display = "inline";
   }
   else{
-    sliderBackgroundVideo.style.display = "none";
+    sliderBackgroundVideo.play();
   }
 
 function autoplay(){
