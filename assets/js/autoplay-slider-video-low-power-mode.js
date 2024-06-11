21e474a0831aa269a474dfdfc1d75fd4ea09728f
karimhammamy <159165220+karimhammamy@users.noreply.github.com>
2024-06-12 01:12:28 +0300

    Update autoplay-slider-video-low-power-mode.js

--- assets/js/autoplay-slider-video-low-power-mode.js
+++ assets/js/autoplay-slider-video-low-power-mode.js
@@ -2,4 +2,5 @@ sliderBackgroundVideo = document.getElementById("slider-background-video");
 function autoplay(){
   sliderBackgroundVideo.play();
 }
+window.onload = autoplay;
 window.ontouchstart = autoplay;
