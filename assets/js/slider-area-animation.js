5052392ecdf7081225db392a776b5c848347d290
karimhammamy <159165220+karimhammamy@users.noreply.github.com>
2024-06-22 17:39:23 +0300

    Update slider-area-animation.js

--- assets/js/slider-area-animation.js
+++ assets/js/slider-area-animation.js
@@ -226,7 +226,7 @@ function autoplay(){
   setTimeout(enablePointerEvents,500);
 }
 function cancelLowPowerModeAutoplayFunction(){
-  if(sliderBackgroundVideo.paused === false){
+  if(sliderBackgroundVideo.paused !== false){
     autoplay = "none";
   }
 };
